dbbba93077461f1fa0ada05cee5eb4f06978f38d
Edrian <edrian.reyes@iloverbs.com>
2025-10-14 16:47:06 +0800

    testingss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between">
      <div className="flex items-center gap-3">
       <img
-        src="src/assets/logo.png" 
+        src="../assets/logo.png" 
         alt="Logo"
         className="w-10 h-10 object-contain"
       />
