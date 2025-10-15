b20dcd6a00340b622959bc1c1a6fe27bfc0c4c31
Edrian <edrian.reyes@iloverbs.com>
2025-10-15 08:24:30 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  // base: "/nueva-explore-ph/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/nueva-explore-ph/",
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [react()],
   resolve: {
     alias: {
